c19eec9c7759ece59c60a185e1422364db7fd68a
cornernote <cornernote@gmail.com>
2015-05-28 09:35:53 +0930

    added bootstrap

---
+++ Bootstrap.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace cornernote\giitools;
+
+use yii\base\Application;
+use yii\base\BootstrapInterface;
+
+
+/**
+ * Class Bootstrap
+ */
+class Bootstrap implements BootstrapInterface
+{
+
+    /**
+     * Bootstrap method to be called during application bootstrap stage.
+     *
+     * @param Application $app the application currently running
+     */
+    public function bootstrap($app)
+    {
+        if ($app->hasModule('gii')) {
+            if ($app instanceof \yii\console\Application) {
+                $app->controllerMap['giitools-batch'] = 'cornernote\giiant\commands\BatchController';
+            }
+        }
+    }
+}

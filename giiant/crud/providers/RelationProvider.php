1e7dc9a4162dc18642c753359507cf1dc6de400e
cornernote <cornernote@gmail.com>
2015-05-28 13:35:19 +0930

    add padding

--- giiant/crud/providers/RelationProvider.php
+++ giiant/crud/providers/RelationProvider.php
@@ -6,8 +6,15 @@ use cornernote\giitools\helpers\TabPadding;
 
 class RelationProvider extends \schmunk42\giiant\crud\providers\RelationProvider
 {
+
     public function columnFormat($column, $model)
     {
         return TabPadding::pad(parent::columnFormat($column, $model), 4);
     }
+
+    public function activeField($column)
+    {
+        return TabPadding::pad(parent::activeField($column));
+    }
+
 }

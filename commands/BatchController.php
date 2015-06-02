da4dd8c387c3d56cf537b186e908697f0c016330
cornernote <cornernote@gmail.com>
2015-06-02 12:41:15 +0930

    cleanup batch generator

--- commands/BatchController.php
+++ commands/BatchController.php
@@ -13,125 +13,71 @@ use yii\helpers\Inflector;
 class BatchController extends \schmunk42\giiant\commands\BatchController
 {
 
-    public $tables = [];
-    public $doModel = true;
-    public $doCrud = true;
+    public $interactive = 0;
 
+    public $template = 'gii-tools';
+    public $overwrite = true;
+    public $extendedModels = false;
+    public $modelNamespace = 'app\models';
+    public $modelBaseClass = 'app\db\ActiveRecord';
+    public $crudControllerNamespace = 'app\modules\admin\controllers';
+    public $crudSearchModelNamespace = 'app\models\search';
+    public $crudViewPath = '@admin/views';
+    public $crudPathPrefix = '';
     public $crudProviders = [
         '\cornernote\giitools\giiant\crud\providers\RelationProvider',
         '\cornernote\giitools\giiant\crud\providers\DateTimeProvider',
         '\cornernote\giitools\giiant\crud\providers\CallbackProvider',
         '\cornernote\giitools\giiant\crud\providers\EditorProvider',
     ];
-    public $template = 'gii-tools';
-    public $modelNamespace = 'app\models';
-    public $interactive = 0;
-    public $overwrite = 1;
-    public $extendedModels = 0;
-    public $crudViewPath = '@admin/views';
-    public $crudControllerNamespace = 'app\modules\admin\controllers';
-    public $crudSearchModelNamespace = 'app\models\search';
-    public $enableI18N = 1;
-    public $modelBaseClass = 'app\db\ActiveRecord';
-    public $crudPathPrefix = '';
 
+    /**
+     * Can be removed when this is merged:
+     * https://github.com/schmunk42/yii2-giiant/pull/67
+     */
     public $generateQuery = true;
     public $queryNs = 'app\models\query';
     public $queryBaseClass = 'yii\db\ActiveQuery';
 
     /**
+     * Can be removed when this is merged:
+     * https://github.com/schmunk42/yii2-giiant/pull/67
+     *
      * @inheritdoc
      */
-    public function options($id)
+    public function actionModels()
     {
-        return array_merge(parent::options($id), [
-            'doModel',
-            'doCrud',
-        ]);
-    }
-
-    /**
-     * Run batch process to generate models and CRUDs for all given tables
-     *
-     * @param string $message the message to be echoed.
-     */
-    public function actionIndex()
-    {
-        echo "Running batch...\n";
-
-        $config = $this->getYiiConfiguration();
-        $config['id'] = 'temp';
-
-        if (!$this->tables) {
-            echo "No tables specified.";
-            exit;
-        }
-
         // create models
-        if ($this->doModel) {
-            foreach ($this->tables AS $table) {
-                #var_dump($this->tableNameMap, $table);exit;
-                $params = [
-                    'interactive' => $this->interactive,
-                    'overwrite' => $this->overwrite,
-                    'template' => $this->template,
-                    'ns' => $this->modelNamespace,
-                    'db' => $this->modelDb,
-                    'tableName' => $table,
-                    'tablePrefix' => $this->tablePrefix,
-                    'enableI18N' => $this->enableI18N,
-                    'messageCategory' => $this->messageCategory,
-                    'generateModelClass' => $this->extendedModels,
-                    'modelClass' => isset($this->tableNameMap[$table]) ? $this->tableNameMap[$table] :
-                        Inflector::camelize($table), // TODO: setting is not recognized in giiant
-                    'baseClass' => $this->modelBaseClass,
-                    'tableNameMap' => $this->tableNameMap,
-                    'generateQuery' => $this->generateQuery,
-                    'queryNs' => $this->queryNs,
-                    'queryBaseClass' => $this->queryBaseClass,
-                ];
-                $route = 'gii/giiant-model';
+        foreach ($this->tables AS $table) {
+            #var_dump($this->tableNameMap, $table);exit;
+            $params = [
+                'interactive' => $this->interactive,
+                'overwrite' => $this->overwrite,
+                'template' => $this->template,
+                'ns' => $this->modelNamespace,
+                'db' => $this->modelDb,
+                'tableName' => $table,
+                'tablePrefix' => $this->tablePrefix,
+                'enableI18N' => $this->enableI18N,
+                'messageCategory' => $this->messageCategory,
+                'generateModelClass' => $this->extendedModels,
+                'modelClass' => isset($this->tableNameMap[$table]) ? $this->tableNameMap[$table] :
+                    Inflector::camelize($table), // TODO: setting is not recognized in giiant
+                'baseClass' => $this->modelBaseClass,
+                'tableNameMap' => $this->tableNameMap,
+                'generateQuery' => $this->generateQuery,
+                'queryNs' => $this->queryNs,
+                'queryBaseClass' => $this->queryBaseClass,
+            ];
+            $route = 'gii/giiant-model';
 
-                $app = \Yii::$app;
-                $temp = new \yii\console\Application($config);
-                $temp->runAction(ltrim($route, '/'), $params);
-                unset($temp);
-                \Yii::$app = $app;
-            }
+            $app = \Yii::$app;
+            $temp = new \yii\console\Application($this->appConfig);
+            $temp->runAction(ltrim($route, '/'), $params);
+            unset($temp);
+            \Yii::$app = $app;
         }
 
-
-        // create CRUDs
-        if ($this->doCrud) {
-            $providers = ArrayHelper::merge($this->crudProviders, Generator::getCoreProviders());
-            foreach ($this->tables AS $table) {
-                $table = str_replace($this->tablePrefix, '', $table);
-                $name = isset($this->tableNameMap[$table]) ? $this->tableNameMap[$table] : Inflector::camelize($table);
-                $params = [
-                    'interactive' => $this->interactive,
-                    'overwrite' => $this->overwrite,
-                    'template' => $this->template,
-                    'modelClass' => $this->modelNamespace . '\\' . $name,
-                    'searchModelClass' => $this->crudSearchModelNamespace . '\\' . $name . 'Search',
-                    'controllerClass' => $this->crudControllerNamespace . '\\' . $name . 'Controller',
-                    'viewPath' => $this->crudViewPath,
-                    'pathPrefix' => $this->crudPathPrefix,
-                    'tablePrefix' => $this->tablePrefix,
-                    'enableI18N' => $this->enableI18N,
-                    'messageCategory' => $this->messageCategory,
-                    'actionButtonClass' => 'yii\\grid\\ActionColumn',
-                    'baseControllerClass' => $this->crudBaseControllerClass,
-                    'providerList' => implode(',', $providers),
-                    'skipRelations' => $this->crudSkipRelations,
-                ];
-                $route = 'gii/giiant-crud';
-                $app = \Yii::$app;
-                $temp = new \yii\console\Application($config);
-                $temp->runAction(ltrim($route, '/'), $params);
-                unset($temp);
-                \Yii::$app = $app;
-            }
-        }
     }
 
 }

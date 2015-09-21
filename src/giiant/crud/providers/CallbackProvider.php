036e35f9590e94365619c6c0c3d2d1d96654d191
cornernote <cornernote@gmail.com>
2015-09-21 10:17:14 +0930

    updates for giiant changes

--- src/giiant/crud/providers/CallbackProvider.php
+++ src/giiant/crud/providers/CallbackProvider.php
@@ -4,7 +4,7 @@ namespace cornernote\gii\giiant\crud\providers;
 
 use cornernote\gii\helpers\TabPadding;
 
-class CallbackProvider extends \schmunk42\giiant\crud\providers\CallbackProvider
+class CallbackProvider extends \schmunk42\giiant\generators\crud\providers\CallbackProvider
 {
 
 }

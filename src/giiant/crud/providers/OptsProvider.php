f43044432bccbe42f9610e0021322249f93803ff
Brett <cornernote@gmail.com>
2016-10-05 21:50:26 +1030

    added OptsProvider.php

---
+++ src/giiant/crud/providers/OptsProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace cornernote\gii\giiant\crud\providers;
+
+use cornernote\gii\helpers\TabPadding;
+
+class OptsProvider extends \schmunk42\giiant\generators\crud\providers\core\OptsProvider
+{
+
+}
